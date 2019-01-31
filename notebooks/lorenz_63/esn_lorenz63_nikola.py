a089f16fc9b30f7896d23ef2d581c34d766764c1
Nikola Jovanovic <jovanovicn.96@gmail.com>
2019-01-31 11:32:48 -0600

    lorentz 63 with number of reservoirs as command line argument

---
+++ notebooks/lorenz_63/esn_lorenz63_nikola.py
@@ -0,0 +1,78 @@
+import pyESN
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import genfromtxt
+import sys
+
+if len(sys.argv) != 2:
+  print("bad command line argumentas - pass the number of reservoirs")
+  exit(1)
+
+reservoirs = int(sys.argv[1])
+
+data=genfromtxt('lorenz_63_modified.csv', delimiter=",")
+dataT=np.transpose(data)
+dataT=np.transpose(dataT)
+# dataT=dataT[2500:,:]
+np.shape(dataT)
+
+esn = pyESN.ESN(
+  n_inputs=1,
+  n_outputs=3,
+  n_reservoir=reservoirs,
+  spectral_radius=0.9,
+  sparsity=0.002,
+  random_state=42)
+
+spin_off = 0
+trainN = 40000
+testN = 2000
+sol_matrix = np.zeros([testN, 3])
+np.shape(sol_matrix)
+
+
+pred_training = esn.fit(np.ones(trainN - spin_off), dataT[spin_off:trainN,:], inspect=True)
+
+prediction = esn.predict(np.ones(testN))
+
+error_mat=prediction-dataT[trainN:trainN + testN]
+
+print('RMSE:')
+print((np.sum(np.sum(np.square(error_mat)))) / float(np.sum(np.sum(np.square(dataT[trainN:trainN + testN])))))
+
+# np.save('prediction_40000', prediction)
+
+v = 0.05 * np.arange(0, 2000)
+
+ptr = 0
+plt.figure(figsize=(15, 1.5))
+plt.plot(v,dataT[trainN:trainN + testN,ptr],'k', label="target system")
+plt.plot(v,prediction[:,ptr], 'r', label="free running ESN")
+lo, hi = plt.ylim()
+plt.plot([0.0, 0.0],[lo + np.spacing(1), hi - np.spacing(1)], 'k:')
+plt.legend(loc=(0.61, 1.1), fontsize='x-small')
+plt.title('X coordinate')
+plt.xlabel('MTU')
+
+
+ptr = 1
+plt.figure(figsize=(15, 1.5))
+plt.plot(v,dataT[trainN:trainN + testN,ptr], 'k', label="target system")
+plt.plot(v,prediction[:,ptr], 'r', label="free running ESN")
+lo, hi = plt.ylim()
+plt.plot([0, 0], [lo + np.spacing(1), hi - np.spacing(1)], 'k:')
+plt.legend(loc=(0.61, 1.1), fontsize='x-small')
+plt.title('Y coordinate')
+plt.xlabel('MTU')
+
+ptr = 2
+plt.figure(figsize=(15, 1.5))
+plt.plot(v,dataT[trainN:trainN + testN,ptr], 'k', label="target system")
+plt.plot(v,prediction[:,ptr], 'r', label="free running ESN")
+lo, hi = plt.ylim()
+plt.plot([0, 0], [lo + np.spacing(1), hi - np.spacing(1)], 'k:')
+plt.legend(loc=(0.61, 1.1), fontsize='x-small')
+plt.title('Z coordinate')
+plt.xlabel('MTU')
+
+# plt.show()
